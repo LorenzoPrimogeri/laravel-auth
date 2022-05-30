e18325d01b55c33e5ede6f1729ba2a29c1a26c57
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-05-30 17:02:05 +0200

    create post table

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,6 @@ Route::middleware('auth')
         Route::get('/', 'HomeController@index')
             ->name('home');
     });
+Route::get("{any?}", function () {
+    return view('guest.home');
+})->where('any', '.');
